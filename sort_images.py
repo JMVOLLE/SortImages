436f9b5093f8ed25f7bb72bc35fc3031ad8445cc
JMV <jeanmarc.volle@gmail.com>
2016-04-02 17:19:53 +0200

    wip

--- sort_images.py
+++ sort_images.py
@@ -7,35 +7,38 @@ import os
 import tkinter
 
 # some constants
-key_date = 'Image DateTime'
 
+KEY_DATE = 'Image DateTime'
 
 def get_capture_date(file_name):
-    # read the exif data from the file and report the image capture date
-    file = open(file_name, 'rb')
+    """
+    :param file_name: name of a jpeg file
+    :return: date a which the photo was taken
+    """
+    with open(file_name, 'rb') as _file:
 
-    # Return Exif tags
-    exif_tags = exifread.process_file(file)
+        # Return Exif tags
+        exif_tags = exifread.process_file(file)
 
-    if key_date in exif_tags.keys():
-        # print("Key: <%s>, value <%s>" % (key_date, exif_tags[key_date]))
-        str_creation_date = str(exif_tags[key_date])
-#        print('Creation date ', str_creation_date)
-        capture_date = datetime.datetime.strptime(str_creation_date, '%Y:%m:%d %H:%M:%S')
-#        print("year:%.4d" % creation_date.year)
-#        print("month:%.2d" % creation_date.month)
+        if KEY_DATE in exif_tags.keys():
+            # print("Key: <%s>, value <%s>" % (key_date, exif_tags[key_date]))
+            str_creation_date = str(exif_tags[KEY_DATE])
+    #        print('Creation date ', str_creation_date)
+            capture_date = datetime.datetime.strptime(str_creation_date, '%Y:%m:%d %H:%M:%S')
+#            print("year:%.4d" % creation_date.year)
+#           print("month:%.2d" % creation_date.month)
     return capture_date
 
 
 
 # read all jpg files from a folder
-jpg_folder = 'd:\\test_source\\*.jpg'
-to = 'd:\\sorted'
+src_folder = 'd:\\test_source\\*.jpg'
+dst_folder = 'd:\\sorted'
 
 
-def parse_source_folder(src_folder):
+def parse_source_folder(folder):
 
-    jpg_files = glob.glob(src_folder)
+    jpg_files = glob.glob(folder)
 
     # parse the input directory
     destination_folders = set()
@@ -53,17 +56,14 @@ def parse_source_folder(src_folder):
     return capture_dates, destination_folders
 
 
-def name_output_directories(in_capture_dates):
+def create_destination_folders(dest_root, destination_folders):
+    """ create output directories if needed """
+    for folder in destination_folders:
+        destination_folder = "%s\%s" % (dest_root, folder)
+        if not os.path.exists(destination_folder):
+            print("Creating: %s" % destination_folder)
+            os.makedirs(destination_folder)
 
-
-
-
-# create output directories if needed
-for folder in destination_folders:
-    destination_folder = "%s\%s" % (to, folder)
-    if not os.path.exists(destination_folder):
-        print("Creating: %s" % destination_folder)
-        os.makedirs(destination_folder)
 
 # move files to where they belong
 for file in capture_dates.keys():

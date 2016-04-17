c01ec9a2a8affe6550e7b144783abe0411f5982f
JMV <jeanmarc.volle@gmail.com>
2016-04-17 17:03:13 +0200

    wip

--- sort_images.py
+++ sort_images.py
@@ -10,12 +10,13 @@ import tkinter
 
 KEY_DATE = 'Image DateTime'
 
+
 def get_capture_date(file_name):
     """
     :param file_name: name of a jpeg file
     :return: date a which the photo was taken
     """
-    with open(file_name, 'rb') as _file:
+    with open(file_name, 'rb') as file:
 
         # Return Exif tags
         exif_tags = exifread.process_file(file)
@@ -30,17 +31,22 @@ def get_capture_date(file_name):
     return capture_date
 
 
-
-
-
 def parse_source_folder(folder):
-    """ find jpg files in the input folder"""
+    """ find jpg files in the input folder
+    :param folder: folder to parse
+    :return: destination_folders : dicti of relative destination folder (keyed by file names)
+    :return: unique_folders : set of relative destination folder
+    """
+
     # parse the input directory
     jpg_files = glob.glob(folder+"\\*.jpg")
-    jpg_files = (jpg_files, glob.glob(folder+"\\*.JPG"))
+    jpg_files = jpg_files + glob.glob(folder + "\\*.JPG")
+    jpg_files = jpg_files + glob.glob(folder + "\\*.jpeg")
+
     # return values
     destination_folders = dict()
     unique_folders = set()
+
     for file in jpg_files:
         capture_date = get_capture_date(file)
         destination = "%.4d\%.2d" % (capture_date.year, capture_date.month)
@@ -63,14 +69,25 @@ def create_destination_folders(dest_root, unique_destination_folders):
             os.makedirs(destination_folder)
 
 
-# move files to where they belong
-for file in capture_dates.keys():
-    destination_folder = "%s\%.4d\%.2d" % (to, capture_dates[file].year, capture_dates[file].month)
-    print(" mv %s %s" % (file, destination_folder))
-    # shutil.move(file, destination_folder)
+def move_files(dest_root, destination_folders):
+    """ move files to where they belong
+    :param destination_folders: a file name ordered list of destination folders
+    """
+    for file in destination_folders.keys():
+        destination_folder = "%s\%s" % (dest_root, destination_folders[file])
+        print(" mv %s %s" % (file, destination_folder))
+        # shutil.move(file, destination_folder)
 
 
 
 # read all jpg files from a folder
 src_folder = 'd:\\test_source'
 dst_folder = 'd:\\sorted'
+
+all_folders, unique_folders = parse_source_folder(src_folder)
+print("Folders to create\n")
+print(unique_folders)
+
+
+
+

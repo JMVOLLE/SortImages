3fb11f53e3cbd1c14af7127239ffa0becec49f46
JMV <jeanmarc.volle@gmail.com>
2016-04-17 16:09:39 +0200

    wip

--- sort_images.py
+++ sort_images.py
@@ -31,34 +31,32 @@ def get_capture_date(file_name):
 
 
 
-# read all jpg files from a folder
-src_folder = 'd:\\test_source\\*.jpg'
-dst_folder = 'd:\\sorted'
 
 
 def parse_source_folder(folder):
-
-    jpg_files = glob.glob(folder)
-
+    """ find jpg files in the input folder"""
     # parse the input directory
-    destination_folders = set()
-    capture_dates = dict()
+    jpg_files = glob.glob(folder+"\\*.jpg")
+    jpg_files = (jpg_files, glob.glob(folder+"\\*.JPG"))
+    # return values
+    destination_folders = dict()
+    unique_folders = set()
     for file in jpg_files:
         capture_date = get_capture_date(file)
-        capture_dates[file] = capture_date
         destination = "%.4d\%.2d" % (capture_date.year, capture_date.month)
-        if destination not in destination_folders:
-            destination_folders.add(destination)
+        destination_folders[file] = destination
+        if destination not in unique_folders:
+            unique_folders.add(destination)
 
     print("%d files found" % len(jpg_files))
-    print("%d destinations(s) " % len(destination_folders))
+    print("%d destination folders(s) " % len(unique_folders))
 
-    return capture_dates, destination_folders
+    return destination_folders, unique_folders
 
 
-def create_destination_folders(dest_root, destination_folders):
+def create_destination_folders(dest_root, unique_destination_folders):
     """ create output directories if needed """
-    for folder in destination_folders:
+    for folder in unique_destination_folders:
         destination_folder = "%s\%s" % (dest_root, folder)
         if not os.path.exists(destination_folder):
             print("Creating: %s" % destination_folder)
@@ -71,3 +69,8 @@ for file in capture_dates.keys():
     print(" mv %s %s" % (file, destination_folder))
     # shutil.move(file, destination_folder)
 
+
+
+# read all jpg files from a folder
+src_folder = 'd:\\test_source'
+dst_folder = 'd:\\sorted'
